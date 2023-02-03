db1200c6c58a2e60491094ec8110c061b84c9a26
YukiYama <zx275fire@gmail.com>
2023-02-03 19:35:14 +0900

    feat: toggle half window mode

--- src/pages/Content/modules/styleForHalfWindow.ts
+++ src/pages/Content/modules/styleForHalfWindow.ts
@@ -1,4 +1,3 @@
-
 export const styleForHalfWindow = (): void => {
   // changes to theater mode
   const sizeButton = document.querySelector('button[aria-label="シアターモード（Alt + t）"]');
@@ -16,6 +15,35 @@ export const styleForHalfWindow = (): void => {
   const chats = document.querySelector("div.right-column[aria-label='右カラム']");
   if (chats instanceof HTMLElement) {
     chats.classList.add("half-window-chats");
-    chats.style.height = `${window.innerHeight - window.innerWidth * 9 / 16}px !important`;
   }
 };
+
+const revertHalfWindow = () => {
+  // revert theater mode
+  const sizeButton = document.querySelector('button[aria-label="シアターモードを終了（Alt+t）"]');
+  if (
+    sizeButton instanceof HTMLElement
+  ) {
+    sizeButton.click();
+  }
+  // style video-player element to 100% width
+  const player = document.querySelector("div.persistent-player");
+  if (player instanceof HTMLElement) {
+    player.classList.remove("half-window-player");
+  }
+  // move chat column to bottom
+  const chats = document.querySelector("div.right-column[aria-label='右カラム']");
+  if (chats instanceof HTMLElement) {
+    chats.classList.remove("half-window-chats");
+  }
+}
+
+let activatedHalfWindow = false;
+export const toggleHalfWindowView = () => {
+  if (activatedHalfWindow) {
+    revertHalfWindow();
+  } else {
+    styleForHalfWindow();
+  }
+  activatedHalfWindow = !activatedHalfWindow;
+}
