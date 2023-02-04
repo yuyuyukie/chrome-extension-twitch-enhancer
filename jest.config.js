589781b34e60ed219c6d34d3aa9777e09e9c7546
YukiYama <zx275fire@gmail.com>
2023-02-04 16:56:59 +0900

    chore: add chrome api mock

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,11 @@
 module.exports = {
   collectCoverage: false, // テストのカバレッジ情報出力  テストが遅くなる可能性
   globals: {}, // 全テスト環境で使用できるグローバル変数の配列を指定
-  testEnvironment: 'node', // テスト実行時に使用されるテスト環境
+  testEnvironment: 'jsdom', // テスト実行時に使用されるテスト環境
   testPathIgnorePatterns: ['/node_modules/'], // テストスキップ対象を正規表現パターンの配列で指定
-  verbose: true, // テスト結果の個別レポート出力
+  verbose: true, // テスト結果の個別レポート出力,
+  // Add this line to your Jest config
+  setupFilesAfterEnv: ['./jest.setup.js']
 };
 
 // https://doc.ebichu.cc/jest/docs/ja/configuration.html
